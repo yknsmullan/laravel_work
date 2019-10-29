8e88658d25045d907a1b28859d8b2780ed8d6837
yknsmullan <yknsmullan@gmail.com>
2019-10-29 23:40:52 +0900

    cssを追記 labelタグの記述方法の修正

--- laravel_practice/resources/views/user_page/user.blade.php
+++ laravel_practice/resources/views/user_page/user.blade.php
@@ -4,27 +4,80 @@
   <meta charset="utf-8">
   <title></title>
   @extends('layouts.layout')
+  <style>
+  .content{
+    height: 250px;
+    width: 600px;
+    margin: 0 auto;
+    background: #fff;
+    border: solid #fff 1px;
+    border-radius: 10px;
+    margin-top: 30px;
+  }
+  .content_text{
+    font-size: 17px;
+    background: #aaa;
+    height: 40px;
+    margin: 0;
+    margin-bottom: 20px;
+  }
+
+  form{
+    margin: 0 auto;
+    width: 200px;
+  }
+
+  .item{
+     overflow: hidden;
+     margin-bottom: 20px;
+  }
+   label{
+     float: left;
+     margin-right: 20px;
+     width:135px;
+     padding-left: 10px;
+     font-size: 13px;
+     font-weight: bold;
+   }
+
+  input[type="email"],input[type="password"]{
+   border: solid 1px #333;
+   border-radius: 5px;
+   padding: 10px;
+   font-size: 15px;
+   display: block;
+   margin: 0 auto;
+  }
+
+  input[type="submit"]{
+   background: #444;
+   border: none;
+   color: white;
+   font-size:16px;
+   font-weight:bold;
+   padding: 10px 20px;
+   text-align: center;
+ }
+  </style>
   </head>
 
 
   <body>
     @section('content')
-
   <section class="content">
    <p class="content_text">Login</p>
   <form class="" action="/login" method="post">
 
   <div class="item">
-    <label>E-Mail
-    <input type="email" name="email" value="">
+    <label for="email">E-Mail
+    <input type="email" name="email" id="email">
     </label>
   </div>
   <div class="item">
-    <label>Password
-    <input type="password" name="password" value="">
+    <label for="password">Password
+    <input type="password" name="password" id="password">
     </label>
   </div>
-
   <div class="button">
     <input type="submit" name="submit" value="login">
   </div>
